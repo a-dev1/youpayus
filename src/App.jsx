1bf1b95c3bffe097059060440d975b58819a751d
a-dev1 <aditya.sharma.dev01@gmail.com>
2024-04-11 17:46:46 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <a href="upi://pay?am=21.00&amp;cu=INR&amp;mc=6300&amp;pa=rzpwcbairtelpaymentsbankltd@yesbank&amp;pn=AirtelPaymentsBankltd&amp;tn=AirtelPaymentsBankltd&amp;tr=MCOe8VFU5hyC3a">CLICK TO PAY SUDARSHAN</a>
+      <br />
+      <br />
+      <br />
+      <a href="paytmmp://pay?am=2.00&amp;cu=INR&amp;mc=6300&amp;pa=rzpwcbairtelpaymentsbankltd@yesbank&amp;pn=AirtelPaymentsBankltd&amp;tn=AirtelPaymentsBankltd&amp;tr=MCV5SidUTB9wf3">PAYTM merchant</a>
+      <br />
+      <a href="paytmmp://upi/pay?am=2.00&amp;cu=INR&amp;mc=6300&amp;pa=rzpwcbairtelpaymentsbankltd@yesbank&amp;pn=AirtelPaymentsBankltd&amp;tn=AirtelPaymentsBankltd&amp;tr=MCV5SidUTB9wf3">PAYTM merchant ios chrom</a>
+      <br />
+      <a href="phonepe://pay?pa=9891532913@upi&amp;pn=YourName&amp;tn=Note&amp;am=1&amp;cu=INR">Phonepe</a>
+      <br />
+      <br />
+      <a href="tez://upi/pay?pa=9891532913@upi&amp;pn=YourName&amp;tn=Note&amp;am=1&amp;cu=INR">tez upi //pay?</a>
+      <br />
+      <br />
+      <br />
+      <a href="tez://upi/pay?cu=INR&amp;mc=6300&amp;pa=rzpwcbairtelpaymentsbankltd@yesbank&amp;pn=AirtelPaymentsBankltd&amp;tn=AirtelPaymentsBankltd&amp;tr=MCVUujdFCkClEj&amp;am=21.00">FROM SIT GPAY</a>
+    </div>
+  );
+}
+
+export default App;
